9aeb5aa241b8746b1203ab7b0ae1b6a7c1281f34
Temesgen dora <tdemeke36@gmail.com>
2024-06-22 09:36:03 -0700

    fized 2

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -38,5 +38,7 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
+        if page_size * page > len(self.dataset()):
+            return []
         index = index_range(page, page_size)
         return self.dataset()[index[0]:index[1]]

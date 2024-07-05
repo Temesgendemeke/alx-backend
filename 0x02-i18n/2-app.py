3e93b9c24ea70509980ddf197e4f61a39f5ef40c
Temesgen dora <tdemeke36@gmail.com>
2024-07-05 12:49:32 -0700

    fixed: task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -17,7 +17,8 @@ class Config:
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
     """ get locale"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES']), 200

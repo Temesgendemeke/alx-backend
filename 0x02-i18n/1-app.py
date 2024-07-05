1390f89efcc3010029eec2154545adf8134bf878
Temesgen dora <tdemeke36@gmail.com>
2024-07-05 12:22:47 -0700

    fixed: task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,11 +3,13 @@
 from flask import Flask
 from flask_babel import Babel
 
-
-class Config():
-    """ Config class"""
-    LANGUAGES = ["en", "fr"]
-
-
 app = Flask(__name__)
-babel = Babel(app, Config, default_locale='en', default_timezone='UTC')
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)

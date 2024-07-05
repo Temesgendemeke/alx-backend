57eeb6671822dbb46dc4da13dca4c966cb2f9ef3
Temesgen dora <tdemeke36@gmail.com>
2024-07-05 12:47:58 -0700

    done: task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Module for Task 0"""
+from flask import Flask, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Config class for app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """ get locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
